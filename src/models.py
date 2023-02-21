a171cb3bacefcd8998d0a974138913e937a108de
Nguyen Duc Quy <quynd@tech.est-rouge.com>
2023-02-21 16:54:03 +0700

    update model

--- src/models.py
+++ src/models.py
@@ -7,7 +7,7 @@ class DictUrl(_database.Base):
     id = _sql.Column(_sql.Integer, primary_key=True, index=True)
     url_id = _sql.Column(_sql.String, unique=True, index=True)
     word = _sql.Column(_sql.String, index=True)
-    url = _sql.Column(_sql.String, index=True)
+    url = _sql.Column(_sql.String, unique=True, index=True)
     token = _sql.Column(_sql.String, index=True)
     status_code = _sql.Column(_sql.Integer, default=0)
     error_message = _sql.Column(_sql.String, nullable=True, default="")
